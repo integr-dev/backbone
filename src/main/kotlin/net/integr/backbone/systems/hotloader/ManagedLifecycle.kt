03530f8d7acead89c0b5848a6d76f6cc0cf7c6e6
integr-dev <erik.reitbauer@outlook.de>
2026-01-31 14:57:38 +0100

    Misc fix

--- src/main/kotlin/net/integr/backbone/systems/hotloader/ManagedLifecycle.kt
+++ src/main/kotlin/net/integr/backbone/systems/hotloader/ManagedLifecycle.kt
@@ -4,11 +4,11 @@ import net.integr.backbone.Backbone
 import org.bukkit.event.Listener
 import kotlin.script.experimental.annotations.KotlinScript
 import kotlin.script.experimental.api.ScriptCompilationConfiguration
-import kotlin.script.experimental.api.providedProperties
 import kotlin.script.experimental.jvm.dependenciesFromClassloader
 import kotlin.script.experimental.jvm.jvm
 
 
+@Suppress("JavaIoSerializableObjectMustHaveReadResolve")
 object ScriptConfiguration : ScriptCompilationConfiguration({
     jvm {
         dependenciesFromClassloader(classLoader = Backbone::class.java.classLoader, wholeClasspath = true)
