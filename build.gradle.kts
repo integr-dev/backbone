a74782015d8c43f01d88c15725cb6e3b2f4fbdf5
integr-dev <erik.reitbauer@outlook.de>
2026-02-03 20:26:18 +0100

    Include compiler for later use

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,8 @@ dependencies {
     implementation(kotlin("scripting-jvm-host"))
     implementation(kotlin("scripting-dependencies"))
 
+    implementation(kotlin("compiler-embeddable"))
+
     implementation("org.apache.ivy:ivy:2.5.2")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
